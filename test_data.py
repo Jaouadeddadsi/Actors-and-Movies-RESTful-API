3a023ce84ec0a5bc56ed8d465c8d02f7d1e759a3
Jaouad Eddadsi <jaouadeddadsi2016@gmail.com>
2020-06-14 22:33:53 +0100

    build: write some fake data for testing

---
+++ test_data.py
@@ -0,0 +1,32 @@
+actors = [
+    {
+        'name': 'actor 1',
+        'age': 23,
+        'gender': 'M',
+        'movies': [
+            {
+                'title': 'Movie 1',
+                'release_date': '2015-03-03'
+            },
+            {
+                'title': 'Movie 2',
+                'release_date': '2020-01-08'
+            }
+        ]
+    },
+    {
+        'name': 'actor 2',
+        'age': 50,
+        'gender': 'F',
+        'movies': [
+            {
+                'title': 'Movie 3',
+                'release_date': '2016-05-03'
+            },
+            {
+                'title': 'Movie 4',
+                'release_date': '2010-10-05'
+            }
+        ]
+    }
+]
