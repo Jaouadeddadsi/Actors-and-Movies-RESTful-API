68bc7102429ea8dda3620786215451060c9daf17
Jaouad Eddadsi <jaouadeddadsi2016@gmail.com>
2020-06-14 16:16:14 +0100

    build(flaskr): set up the CORS hreaders for the app

---
+++ flaskr/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from models import setup_db, Movie, Actor
+from flask_cors import CORS
+
+
+def create_app(test_config=None)):
+    # create and configure the app
+    app=Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    # CORS Headers
+    @app.after_request
+    def after_request(reponse):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+
+    return app
